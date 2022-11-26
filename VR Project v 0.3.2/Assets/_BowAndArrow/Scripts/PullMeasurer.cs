f93d0e7a7d7f29aca2fd916e328c2ab4a6fa27bf
NATSURAZOR <ulyan.super2405@gmail.com>
2022-11-26 12:59:11 +0100

    test

---
+++ VR Project v 0.3.2/Assets/_BowAndArrow/Scripts/PullMeasurer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PullMeasurer : XRBaseInteractable
+{
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+
+    public float PullAmount { get; private set; } = 0.0f;
+
+    // lambda operation,
+    public Vector3 PullPosition => Vector3.Lerp(start.position, end.position, PullAmount);
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        PullAmount = 0;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+        // when  hamstring is grab
+        if (isSelected)
+        {
+            // Update pull values while the measurer is grabbed
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+                UpdatePull();
+        }
+    }
+
+    private void UpdatePull()
+    {
+        // Use the interactor's position to calculate amount
+        Vector3 interactorPosition = firstInteractorSelecting.transform.position;
+
+        // Figure out the new pull value, and it's position in space
+        PullAmount = CalculatePull(interactorPosition);
+    }
+
+    private float CalculatePull(Vector3 pullPosition)
+    {
+        // Direction
+        Vector3 pullDirection = pullPosition - start.position;
+        // vector of direction ( from hamstring )
+        Vector3 targetDirection = end.position - start.position;
+
+        // Figure out out the pull direction
+        float maxLength = targetDirection.magnitude;
+        targetDirection.Normalize();
+
+        // What's the actual distance?
+        float pullValue = Vector3.Dot(pullDirection, targetDirection) / maxLength;
+        // Clamp(float value, float min, float max); return number beetwen 0 and 1
+        return Mathf.Clamp(pullValue, 0.0f, 1.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // if start end end is not null than create line between this 2 points
+        if (start && end)
+            Gizmos.DrawLine(start.position, end.position);
+    }
+}
---
+++ VR Project v 0.3.2/Assets/_BowAndArrow/Scripts/StringRenderer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class StringRenderer : MonoBehaviour
+{
+    [Header("Render Positions")]
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform middle;
+    [SerializeField] private Transform end;
+
+    private LineRenderer lineRenderer;
+
+    // Start is called on the frame when a script is enabled just before any of the Update methods are called the first time. Like the Awake function, Start is called exactly once in the lifetime of the script. However, Awake is called when the script object is initialised, regardless of whether or not the script is enabled.
+    private void Awake()
+    {
+   
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        // Application.isEditor - Returns true if the game is being run from the Unity editor; false if run from any deployment target.
+        // Application.isPlaying - Returns true when called in any kind of built Player, or when called in the Editor in Play Mode(Read Only).
+        // In a built Player, this method will always return true.
+        // In the Editor, it will return true if the Editor is in Play Mode.
+        if (Application.isEditor && !Application.isPlaying)
+            UpdatePositions();
+    }
+
+    // This function is called when the object becomes enabled and active.
+    private void OnEnable()
+    {
+        Application.onBeforeRender += UpdatePositions;
+    }
+
+    // This function is called when the behaviour becomes disabled.
+    // This is also called when the object is destroyed and can be used for any cleanup code.When scripts are reloaded after compilation has finished, OnDisable will be called, followed by an OnEnable after the script has been loaded
+    private void OnDisable()
+    {
+        Application.onBeforeRender -= UpdatePositions;
+    }
+
+    private void UpdatePositions()
+    {
+        // Set positions of line renderer, middle position is the notch attach transform
+        lineRenderer.SetPositions(new Vector3[] { start.position, middle.position, end.position });
+    }
+}
