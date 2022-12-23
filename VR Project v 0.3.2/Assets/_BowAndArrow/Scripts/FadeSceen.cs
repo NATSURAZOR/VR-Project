edc5ac38de05919e20fcc8fdea23be3020cb59d0
NATSURAZOR <ulyan.super2405@gmail.com>
2022-12-23 21:00:30 +0100

    swith scene a screen fade

---
+++ VR Project v 0.3.2/Assets/_BowAndArrow/Scripts/FadeSceen.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeSceen : MonoBehaviour
+{
+    public bool fadeOnStart = true;
+    public float fadeDuration = 2;
+    public Color fadeColor;
+
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        if (fadeOnStart)
+        {
+            FadeIn();
+        }
+    }
+
+    public void FadeIn()
+    {
+        Fade(1, 0);
+    }
+
+    public void FadeOut()
+    {
+        Fade(0, 1);
+    }
+
+    public void Fade(float alphaIn, float alphaOut)
+    {
+        
+        StartCoroutine(FadeRoutine(alphaIn, alphaOut));
+    }
+
+    public IEnumerator FadeRoutine(float alphaIn, float alphaOut)
+    {
+        Debug.Log("start");
+        float timer = 0;
+        while(timer <= fadeDuration)
+        {
+            Debug.Log("on while loop");
+            Color newColor = fadeColor;
+            newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
+
+            rend.material.SetColor("_Color", newColor);
+
+            timer += Time.deltaTime;
+
+            
+            yield return null;
+        }
+
+        Color newColor2 = fadeColor;
+        newColor2.a = alphaOut;
+
+        rend.material.SetColor("_Color", newColor2);
+    }
+}
